081a3db843a262479dd70dde0e18778cce8ace3c
jslovejs182 <jslovejs182@gmail.com>
2022-07-28 17:39:38 +0900

    스프링 빈 조회 테스트 코드 (다양한 방법으로) - ApplicationContextBasicFindTest.java  > 빈 이름으로  > 이름 없이 타입으로 조회  > 구현 타입으로 조회  > 등록된 빈 이름이 없는 경우 - ApplicationContextSameBeanFindTest.java  > 동일한 빈을 조회 시 오류  > 같은 타입이 둘 이상 있을 경우 빈 이름으로 조회  > 특정 타입을 모두 조회

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,11 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository;
-
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
+    private final MemberRepository memberRepository;
 
     @Override
     public void join(Member member) {
